107e361b067ed8605e23471892b75b4d5f0f394d
Philipp Kewisch <mozilla@kewis.ch>
2015-06-27 22:11:28 +0200

    BYDAY performance/code quality improvements

--- test/recur_iterator_test.js
+++ test/recur_iterator_test.js
@@ -127,13 +127,12 @@ suite('recur_iterator', function() {
         by_iter: {
           BYMONTH: { arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], idx: 0, wrapped: false },
           BYMONTHDAY: { arr: [1], idx: 0, wrapped: true },
-          BYDAY: { arr: ["MO", "TU", "WE", "TH", "FR", "SA", "SU"], idx: 0, wrapped: false },
+          BYDAY: { arr: [[0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 1]], idx: 0, wrapped: false },
           BYHOUR: { arr: [9], idx: 0, wrapped: true },
           BYMINUTE: { arr: [0], idx: 0, wrapped: true },
-          BYSECOND: { arr: [0], idx: 0, wrapped: true },
+          BYSECOND: { arr: [0], idx: 0, wrapped: true }
         },
 
-        by_data_byday: [[0, "MO"], [0, "TU"], [0, "WE"], [0, "TH"], [0, "FR"], [0, "SA"], [0, "SU"]],
         by_cache_year: 0,
         by_cache: []
       };
